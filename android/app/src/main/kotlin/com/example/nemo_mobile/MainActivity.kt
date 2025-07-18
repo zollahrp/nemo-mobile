d60e7f798a7bda99af30ca1dfdb4d2d2d1076115
zollahrp <zollastarharahap@apps.ipb.ac.id>
2025-07-18 19:19:59 +0700

    struktur proyek

---
+++ android/app/src/main/kotlin/com/example/nemo_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nemo_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nemo Mobile',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nemo.AI'),
+      ),
+      body: const Center(
+        child: Text('Halo dunia, ini Nemo versi mobile!'),
+      ),
+    );
+  }
+}
